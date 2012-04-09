015f34ae3f4dcdc1e9b40ac3e72e95613dfbf1f9
Sho Hashimoto <hashimoto@shokai.org>
2012-04-09 12:02:59 +0900

    fix bootstrap

--- bootstrap.rb
+++ bootstrap.rb
@@ -2,14 +2,14 @@ require 'rubygems'
 require 'yaml'
 require 'rainbow'
 
+@@conf = YAML::load open(File.dirname(__FILE__)+'/config.yaml')
+
 [:helper].each do |cat|
   Dir.glob(File.dirname(__FILE__)+"/#{cat}/*").each do |f|
     puts "loading #{f}"
     require f
   end
 end
-
-@@conf = YAML::load open(File.dirname(__FILE__)+'/config.yaml')
 
 if __FILE__ == $0
   puts 'ok'
